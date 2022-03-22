814caac5a54e0db34e78a26a3975fd784b172580
Carlos Camponuevo <carlos@ipremios.com>
2022-03-21 22:35:48 -0300

    importando tudo

---
+++ src/main/kotlin/br/com/curso/controller/VeiculoController.kt
@@ -0,0 +1,24 @@
+package br.com.curso.controller
+
+import br.com.curso.model.Veiculo
+import br.com.curso.service.VeiculoService
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.*
+
+
+@Controller
+class VeiculoController (
+    private val veiculoService: VeiculoService
+){
+
+    @Post
+    fun create(@Body veiculo: Veiculo): HttpResponse<Veiculo> {
+        return HttpResponse.created(veiculoService.create(veiculo))
+    }
+
+    @Get("/{id}")
+    fun findById(@PathVariable id: Long): Veiculo {
+       return veiculoService.findById(id)
+    }
+
+}
